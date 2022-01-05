77a202b108c9416805c070f5e25bc75179578883
Sai Meher Borra <f20190142@pilani.bits-pilani.ac.in>
2022-01-05 22:15:18 +0530

    light changes

--- attribute_parser.cpp
+++ attribute_parser.cpp
@@ -11,15 +11,17 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
     int n,q;
     string lines[200];
+    //vector<string> lines;
     printf("Enter the values of n and q\n");
-    //scanf("%d %d", &n,&q);
-    scanf("%d",&n);
+    scanf("%d %d", &n,&q);
+    //scanf("%d",&n);
     for(int i = 0; i < n; i++){
+        string temp;
             printf("Enter the line\n");
-            getline(cin,lines[i]);
+            getline(cin >> ws,lines[i]);
             cout << ">" << lines[i] <<endl;
     }
-
+    
 
     return 0;
 }
