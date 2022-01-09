73873a9fe8c238c19491580e55ed267f844b0d4c
Sai Meher Borra <f20190142@pilani.bits-pilani.ac.in>
2022-01-09 20:47:36 +0530

    changes

--- practice.py
+++ practice.py
@@ -1,4 +1,2 @@
 wardrobe = {"shirt":["red","blue","white"], "jeans":["blue","black"]}
-for element in wardrobe:
-    for colour in wardrobe[element]:
-        print("{} {}".format(colour,element))
+print(wardrobe)
